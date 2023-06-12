626bd41ef46330ffa541e1a476419de59a4ad460
vitu <vitorcsharp@hotmail.com>
2023-06-12 15:38:05 -0300

    adding an error when it gives an error 400, but now we remove the body of the error and return in which field the error occurred in a customized way, I just need to study how to change the message that spring already gives

--- src/main/java/com/remedios/vitu/Remedios/handler/TratarErros.java
+++ src/main/java/com/remedios/vitu/Remedios/handler/TratarErros.java
@@ -3,8 +3,12 @@ package com.remedios.vitu.Remedios.handler;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+//Essa classe vai dar uma resposta ao erro !!
 
 @RestControllerAdvice // -> reponsavel por toda vez que acontecer uma exception no controller, essa classe vai ser chamada para ver se estamos tratando o erro
 public class TratarErros {
@@ -15,5 +19,28 @@ public class TratarErros {
         //ResponseEntity<?> -> o <?> esta informando que é é generico igual no dart o T, ou seja qualquer tipo vai poder ser usado
         return ResponseEntity.notFound().build();
         //notFound -> vai retornar 404 not found
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    //MethodArgumentNotValidException -> quando alguem manda uma validação errada
+    //a gente tem algumas validações no nosso DTO
+    //se vier alguma errada vai cair aqui nessa exception
+    public ResponseEntity<?> tratarErro400(MethodArgumentNotValidException ex) {
+        //para retornar uma mensagem personalizada com o erro especifico, vamos colocar o MethodArgumentNotValidException como parametro
+        //getFieldErrors -> vai ver todos os campos e qual campo deu o erro (ele gera uma lista)
+        var erros = ex.getFieldErrors();
+
+        //badRequest -> vai retornar 400 bad request e não vai retornar um 'body' como estava acontecendo
+        return ResponseEntity.badRequest().body(erros.stream()
+                .map(DadosError::new) //-> para utilizar essa lambda a classe chamada precisa ter um construtor
+                .toList());
+    }
+
+    public record DadosError(String field, String message) {
+        //Esse dto esta sendo criado dentro dessa classe pois ele só será utilizado aqui dentro !!!
+
+        public DadosError(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
